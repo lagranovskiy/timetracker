ed3313438d1f5f7671dfbb812be52addeace32e6
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2015-04-05 02:10:15 +0200

    Form for creating / updating projects

--- app/scripts/controllers/management.js
+++ app/scripts/controllers/management.js
@@ -5,10 +5,11 @@
  * Project board controller
  */
 angular.module('timetrackerApp.controller.management', [])
-  .controller('ManagementCtrl', function($scope, $q, $location, ProjectModel, PersonModel) {
+  .controller('ManagementCtrl', function($scope, $q, $log, $location, ProjectModel, PersonModel) {
 
     $scope.projects = [];
-    $scope.selectedProject = {};
+    $scope.selectedProject = {}; // Project that selected in the list
+    $scope.currentProject = null; // Project for creation / update
 
     $scope.personMap = {};
 
@@ -17,14 +18,68 @@ angular.module('timetrackerApp.controller.management', [])
     $scope.projectResources = null;
     $scope.projectBookings = null;
 
+
+    /**
+     * Create a new project booking
+     * */
+    $scope.createProject = function() {
+      if (!$scope.currentProject) {
+        $log.debug('Cannot create null project');
+        return;
+      }
+      ProjectModel.resource.save($scope.currentProject).$promise
+
+        .then(function() {
+        return $scope.refreshProjects();
+      }, $scope.showError)
+
+      .then(function() {
+        $scope.currentProject = null;
+      }, $scope.showError);
+
+    };
+
+
+
+    /**
+     * Create a new project booking
+     * */
+    $scope.updateProject = function() {
+      if (!$scope.currentProject || !$scope.currentProject.id) {
+        $log.debug('Cannot update non persistent project');
+        return;
+      }
+      ProjectModel.resource.update($scope.currentProject).$promise
+        .then(function() {
+          return $scope.refreshProjects();
+        }, $scope.showError)
+        .then(function() {
+          $scope.currentProject = null;
+        }, $scope.showError);
+    };
+
+
+
+
+    /**
+     * Creates instance for a new project
+     */
+    $scope.produceNewProject = function() {
+      $scope.currentProject = ProjectModel.produceNewProject();
+    };
+
     $scope.go = function(path) {
       $location.path(path);
     };
+
 
     /**
      * Returns the time in hours how much time booked on project
      */
     $scope.getTimeBooked = function() {
+      if (!$scope.projectStatistics) {
+        return null;
+      }
       return moment.duration($scope.projectStatistics.bookedTime).asHours() - moment.duration($scope.projectStatistics.bookedPause, 'minutes').asHours();
     };
 
@@ -59,22 +114,37 @@ angular.module('timetrackerApp.controller.management', [])
     };
 
 
+
+
     /**
-     * Init Controller
+     * Initializes the booking page with start data
      */
     $scope.init = function() {
-      /**
-       * Load visible projects by page load
-       * */
-      ProjectModel.resource.query(function(data) {
+
+      $q.all([
+
+        $scope.refreshProjects(),
+
+        /**
+         * Load user bookings
+         */
+        PersonModel.resource.query(function(data) {
+          $scope.personMap = _.indexBy(data, 'id');
+        })
+
+      ]);
+
+    };
+
+    /**
+     * Refreshes bookings of the current user
+     * */
+    $scope.refreshProjects = function() {
+      return ProjectModel.resource.query(function(data) {
         if (data) {
           $scope.projects = data;
           $scope.selectProject(data[0]);
         }
-      });
-
-      PersonModel.resource.query(function(data) {
-        $scope.personMap = _.indexBy(data, 'id');
       });
     };
 
