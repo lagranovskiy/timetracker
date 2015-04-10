c27fbc8b849ec5c2836e0b247ed026d3c4601f97
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2015-04-10 02:26:58 +0200

    Admin interface started

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -4,8 +4,31 @@
  * Project board controller
  */
 angular.module('timetrackerApp.controller.admin', [])
-  .controller('AdminCtrl', function($scope, ProjectModel) {
+  .controller('AdminCtrl', function($scope, $q,PersonModel) {
+
+      $scope.personList = [];
+
+      /**
+       * Initializes the booking page with start data
+       */
+      $scope.init = function() {
+
+        $q.all([
+
+        /**
+         * Load person list
+         */
+          PersonModel.resource.query(function(data) {
+            $scope.personList = data;
+          }),
 
 
 
-  });
+
+        ]);
+
+      };
+
+
+
+    });
